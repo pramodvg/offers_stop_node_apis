f932c907405324b9eb2bddd1dac843a24e6824ad
Pramod Vaghasia <pridetoracer@gmail.com>
2022-03-13 17:26:51 -0400

    a

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(logger('dev'));
 
 // view engine setup
-app.set('views', path.join(__dirname, 'View'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 app.use(cookieParser());
